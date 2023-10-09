ff3f08dc3e2cb90cf0365c129b4f26b452490360
MPahril <pahrilm710@gmail.com>
2023-10-09 17:30:16 +0800

    first commit

---
+++ Day1.java
@@ -0,0 +1,27 @@
+package day1;
+public class Day1 {
+    public static void main(String[] args) {
+       String nama,alamat,email;
+       int usia;
+       double tinggi,berat;
+       boolean status_pernikahan;
+       
+       nama = "M.Pahril";
+       alamat = "Campalagian";
+       email = "pahrilm710@gmail.com";
+       usia = 20;
+       tinggi = 178.4;
+       berat = 98.5;
+       status_pernikahan = false;
+       
+       System.out.println("Nama              : "+nama);
+       System.out.println("Alamat            : "+alamat);
+       System.out.println("Email             : "+email);
+       System.out.println("Usia              : "+usia);
+       System.out.println("Tinggi            : "+tinggi);
+       System.out.println("Berat             : "+berat);
+       System.out.println("Status Pernikahan : "+status_pernikahan);
+               
+    }
+    
+}
