6b887e0c4198989b54be5599b753c7d97c99891f
MPahril <147221778+MPahril@users.noreply.github.com>
2023-10-21 22:23:18 +0800

    ISC_Day13_Perulangan

---
+++ Day13_perulangan.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Day13_perulangan {
+    public static void main(String[] args) {
+        for(int i = 5; i<=21;i++){
+            System.out.println("nomor "+i);
+        }
+    }
+}
