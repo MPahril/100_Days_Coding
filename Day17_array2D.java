fd9d8fd5ca9d97d816d1d5b6119fc0b4fd984187
MPahril <147221778+MPahril@users.noreply.github.com>
2023-10-25 20:54:18 +0800

    ISC_Day17_Array2D

---
+++ Day17_array2D.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Day17_array2D {
+    public static void main(String[] args) {
+        int [][]ryl = new int [2][2];
+         
+        ryl[0][0] = 21;
+        ryl[0][1] = 24;
+        ryl[1][0] = 10;
+        ryl[1][1] = 82;
+        System.out.println("Variabel Array nya Adalah : ");
+        System.out.println(ryl[0][0]+"-_-"+ryl[0][1]);
+        System.out.println(ryl[1][0]+"-_-"+ryl[1][1]);
+    }
+}
