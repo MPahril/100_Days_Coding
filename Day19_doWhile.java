0f780a08a6f54aeda02678ff8f52b40fc2f97345
MPahril <147221778+MPahril@users.noreply.github.com>
2023-10-27 17:20:27 +0800

    ISC_Day19_Belajar do While

---
+++ Day19_doWhile.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Day19_doWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Nama Saya Pahril"); 
+        }
+        while (i <= 1000);
+    }
+}
