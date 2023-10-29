7a64c1ab790d4f95bd407765a3e6415fcc5b157c
MPahril <147221778+MPahril@users.noreply.github.com>
2023-10-29 20:04:27 +0800

    ISC_Day21_BelajarPerulangan

---
+++ Day21_belajarPerulangan.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Day21_belajarPerulangan {
+    public static void main(String[] args) {
+        for(int i=1; i <=100; i++){
+            System.out.println(i+"");
+        }
+    }
+}
