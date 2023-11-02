d6160afcac7e5dea28e1977a729b40369fa2a0c8
MPahril <147221778+MPahril@users.noreply.github.com>
2023-11-02 19:51:27 +0800

    ISC_Day25_Belajar Perulangan

---
+++ Day25_Perulangan.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Day25_Perulangan {
+    public static void main(String[]args){        
+        for(int i=8;i>6;i++){
+            for(int j=8;j>6;j++){
+                System.out.println(i+" |-_-| "+j);
+            }
+            System.out.println("");
+        }        
+    }
+}
+
