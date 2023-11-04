dabd218d27abcc12dba68b1db4d3cadeb8aedb87
MPahril <147221778+MPahril@users.noreply.github.com>
2023-11-04 22:07:42 +0800

    ISC_Day27_BelajarPerulangan

---
+++ Day27_Perulangan.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Day27_Perulangan {
+    public static void main(String[] args) {
+       int i = 2;
+        while(i <= 100){
+            if (i % 3 != 0) {
+                System.out.println(i);
+            }
+            i+=2;
+        }
+                
+    }
+}
