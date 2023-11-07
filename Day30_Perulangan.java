85f3e3d0287b58bd34bc77f657f5739ff2193cbb
MPahril <147221778+MPahril@users.noreply.github.com>
2023-11-07 18:58:11 +0800

    ISC_Day30_belajarPerulangan

---
+++ Day30_Perulangan.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Day30_Perulangan {
+    public static void main(String[] args) {
+        for (int i = 6; i <= 200; i+= 2) {
+            System.out.println(i);
+        }
+    }
+}
