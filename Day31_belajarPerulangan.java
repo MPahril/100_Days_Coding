16da876c314de90677fe17acd69f1b4ad2abb411
MPahril <147221778+MPahril@users.noreply.github.com>
2023-11-08 20:06:21 +0800

    ISC_Day31_belajarPerulangan

---
+++ Day31_belajarPerulangan.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Day31_belajarPerulangan {
+    public static void main(String[] args) {
+        for (int i =2; i <= 200; i+= 2) {
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+ 
+        }
+    }
+}
