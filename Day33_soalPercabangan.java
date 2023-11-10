271a06c16d4c91daa42933c652c626f4e5f90d6f
MPahril <147221778+MPahril@users.noreply.github.com>
2023-11-10 21:15:48 +0800

    Update Day33_soalPercabangan.java

--- Day33_soalPercabangan.java
+++ Day33_soalPercabangan.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
 public class Day33_soalPercabangan {
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
+       while(true){
         System.out.print("Masukkan angka: ");  
         int angka = scanner.nextInt();
         if (angka % 3 == 0 && angka % 5 == 0) {
             System.out.println("Master Class");
+            break;
          }else if (angka % 3 == 0) {
             System.out.println("Pride of 3");
+            break;
        }else if (angka % 5 == 0) {
             System.out.println("Pride of 5");
+            break;
         }else{
             System.out.println("Angka tidak dapat di bagi 3 atau 5");
+          
         }
          }
         }
-
+}
