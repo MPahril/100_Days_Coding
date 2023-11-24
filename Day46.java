79f923a6bab915fba11e06d25ce15dca0e3b19c2
MPahril <147221778+MPahril@users.noreply.github.com>
2023-11-24 20:12:08 +0800

    ISC_Day46

---
+++ Day46.java
@@ -0,0 +1,25 @@
+
+package day1;
+
+import java.util.Scanner;
+
+public class Day46 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Masukkan A : ");
+        int a = sc.nextInt();
+        System.out.print("Masukkan B : ");
+        int b = sc.nextInt();
+        
+        int c  = a+b;
+        int d;
+        
+        if (c %2 == 0 && c- 3!=0) {
+            d = c*2;
+        }else{
+            d = c +2;
+            
+        }
+        System.out.println(d);
+    }
+}
