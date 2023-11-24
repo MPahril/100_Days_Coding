3b4b18d74ed59c79cac9567a7453a23eba223466
MPahril <147221778+MPahril@users.noreply.github.com>
2023-11-24 20:37:29 +0800

    ISC_Day47

---
+++ Day47.java
@@ -0,0 +1,16 @@
+package day1;
+
+import java.util.LinkedList;
+
+public class Day47 {
+    public static void main(String[] args) {
+        LinkedList<String> rill = new LinkedList<String>();
+        
+        rill.add("Pahril");
+        rill.add("Pahrul");
+        rill.add("Pahrel");
+        rill.add("Pahrol");
+        
+        System.out.print(rill);
+    }
+}
