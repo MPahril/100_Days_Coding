bb0d07dce9d0c01942bcc298d5ba8248092e6c33
MPahril <147221778+MPahril@users.noreply.github.com>
2023-11-25 20:30:31 +0800

    ISC_Day48

---
+++ Day48.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Day48 {
+    public static void main(String[] args) {
+        int [] vektorA = {2, 1, 5};
+        int [] vektorB = {5, 4, 2};
+        
+        
+        for (int i = 0; i < vektorA.length; i++) {
+            System.out.println(vektorA[i] + vektorB[i]+" ");
+        }
+        System.out.println();
+    }
+ 
+}
