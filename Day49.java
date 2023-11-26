7c0b91160979a9d6affb4a05d46ba9e4bad55dbc
MPahril <147221778+MPahril@users.noreply.github.com>
2023-11-26 22:23:59 +0800

    ISC_Day49

---
+++ Day49.java
@@ -0,0 +1,21 @@
+package day1;
+
+import java.util.HashMap;
+
+public class Day49 {
+    public static void main(String[] args) {
+        HashMap<Integer, String> rill = new HashMap <Integer, String>();
+        
+        
+        rill.put (1, "Pahril");
+        rill.put(2, "Cumi");
+        rill.put(3, "ChekyyN");
+        rill.put(4, "Ril");
+        rill.put(5, "Pian");
+        
+        System.out.println(": Daftar Nama Manusia : ");
+        System.out.println(rill);
+        System.out.println();
+    }
+    
+}
