2cc8bc8a523b4fb79a6590614f06bd199643f501
MPahril <147221778+MPahril@users.noreply.github.com>
2023-12-03 21:34:57 +0800

    ISC_Day56

---
+++ Day56.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Day56 {
+    public static void main(String[] args) {
+        // Mendefinisikan dua vektor A dan B
+        int[] vektorA = {26, 19, 59};
+        int[] vektorB = {51, 47, 24};
+        
+        for (int i = 0; i < vektorA.length; i++) {
+        System.out.println(vektorA[i] + vektorB[i] + " ");//Menjumlahkan vektorA dan VektorB
+        }
+        System.out.println();//mencetak baris baru
+    }
+}
