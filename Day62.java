5dbd5def80ae66658f8f7bcf5b529bbbbad1a68f
MPahril <147221778+MPahril@users.noreply.github.com>
2023-12-09 23:21:30 +0800

    ISC_Day62

---
+++ Day62.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class Day62 {
+    public static void main(String[] args) {
+        String[] teman;
+ 
+   teman = new String[3];
+     
+    teman[0] = "Utii";
+    teman[1] = "Rill";
+    teman[2] = "Cumii";
+     
+    System.out.println( teman[0] );  
+    System.out.println( teman[1] );
+    System.out.println( teman[2] );
+ 
+  }
+}
+
