1b7a71c9abf859b983dcceed166815ac7e4b76f4
MPahril <147221778+MPahril@users.noreply.github.com>
2023-12-15 00:16:33 +0800

    ISC_Day67

---
+++ Day67.java
@@ -0,0 +1,24 @@
+package day1;
+
+import java.util.Scanner;
+
+public class Day67 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String[] manusia = new String[3];
+        
+        System.out.println(": Masukkan Nama Teman Anda : ");
+        
+        System.out.print("Nama Teman Pertama: ");
+        manusia[0] = sc.nextLine();
+        System.out.print("Nama Teman Kedua  : ");
+        manusia[1] = sc.nextLine();
+        System.out.print("Nama Teman Ketiga : ");
+        manusia[2] = sc.nextLine();
+        
+        System.out.print("Nama Nama Teman Anda : ");
+        System.out.println(manusia[0]+", "+manusia[1]+", "+manusia[2]);
+    }
+}
+
+ 
