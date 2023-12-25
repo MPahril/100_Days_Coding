9d74ab1338ae456cfd04e1cab7ce0dfc7d760473
MPahril <147221778+MPahril@users.noreply.github.com>
2023-12-25 20:43:59 +0800

    ISC_Day78

---
+++ Day78.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package day1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Day78 {
+    public static void main(String[] args) {
+        
+    }
+}
