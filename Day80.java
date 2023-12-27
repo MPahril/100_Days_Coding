e87558582d3f2c80617a69e8e369d65eef7e62c0
MPahril <147221778+MPahril@users.noreply.github.com>
2023-12-27 20:27:16 +0800

    ISC_Day80

---
+++ Day80.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package day1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Day80 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList arr = new ArrayList();
+        System.out.println("=== Daftar Nama Nama Hewan ===");
+        arr.add("ayam");
+        arr.add("bebek");
+        arr.add("cicak");
+        arr.add("domba");
+        arr.add("elang");
+        arr.add("gajah");
+        arr.add("harimau");
+        arr.add("ikan");
+        arr.add("jerapa");
+        arr.add("gurita");
+
+        System.out.println(arr);
+    }
+}
