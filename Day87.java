73a7cb5c35b7e9d7af1279be1a3f908362467174
MPahril <147221778+MPahril@users.noreply.github.com>
2024-01-03 19:46:06 +0800

    ISC_Day87

---
+++ Day87.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package day1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Day87 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+            if(i==21){
+                break;
+            } 
+        }
+    }
+}
