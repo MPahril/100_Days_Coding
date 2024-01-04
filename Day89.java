33113d8def76ece6b62221de415ad0db7bc34cba
MPahril <147221778+MPahril@users.noreply.github.com>
2024-01-05 05:16:44 +0800

    ISC_Day89

---
+++ Day89.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package day1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Day89 {
+ 
+    public static void Pahril(){
+        System.out.println("Pahriiiiiiiiiiiiil");
+    }
+    public static void main(String[] args) {
+        while(true){
+            Pahril();
+        }
+    }
+}
+    
